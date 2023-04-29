e333781fc54eac85281fc88ecd02788f5f6b4604
brenolins100 <brenolins100@gmail.com>
2023-04-29 15:00:29 -0300

    Alterando porta e host

--- server.js
+++ server.js
@@ -26,11 +26,12 @@ fastify.register(require('./routes/ProductRoutes'), { prefix: '/api' });
 // Iniciar o servidor
 const start = async () => {
   try {
-    await fastify.listen(3000, '0.0.0.0' );
-    console.log('Server running at http://localhost:3000/');
+    await fastify.listen({ port: 3000, host: '0.0.0.0' });
+    console.log('Server running at http://0.0.0.0:3000/');
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
   }
 };
 start();
+
